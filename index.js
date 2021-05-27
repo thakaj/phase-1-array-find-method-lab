952f6566f36c7375cfc8c4290f7bdc46a5b8dfd9
thakaj <taulant.hakaj@scranton.edu>
2021-05-27 12:27:40 -0400

    commit tree 6bf4c63804e3562fb456118518f2db52bba1ed6d

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(arrayRecord){
+    const victory = arrayRecord.find(record => record.result ===`W`);
+    if (victory) {
+        return victory.year
+    } else {
+        
+    }
+}
